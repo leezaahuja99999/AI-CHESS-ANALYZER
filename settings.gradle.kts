a73e9313d8ae0fe837b30b6eb737f950fc8a5d3f
leezaahuja99999 <leezaahuja99999@gmail.com>
2026-09-21 19:16:02 +0530

    Add JitPack repository to dependency management

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,8 @@
 pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories { google(); mavenCentral() }
+    repositories { google(); mavenCentral() 
+maven { url = uri("https://jitpack.io") }
 }
 rootProject.name = "AIChessAnalyzer"
 include(":app")
