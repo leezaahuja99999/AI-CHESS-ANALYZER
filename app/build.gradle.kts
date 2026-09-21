b5051f327d8489e49e9f9ea4c5aa225265329dba
leezaahuja99999 <leezaahuja99999@gmail.com>
2026-09-21 21:13:44 +0530

    Set Java and Kotlin compatibility to version 21
    Update Java and Kotlin compatibility to version 21.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,6 +13,15 @@ android {
         versionName = "3.0"
     }
 }
+
+compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+kotlinOptions {
+    jvmTarget = "21"
+}
 dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
